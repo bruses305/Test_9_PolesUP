9688d40ff73d2ff6d033d135a1db515a745c798d
37533 <37533@LAPTOP-GAM2DSJ2>
2023-04-24 20:31:55 +0300

    Добавьте файлы проекта.

---
+++ Test_9_PolesUP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Test_9_PolesUP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.Write("Введите год : ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a % 4 == 0 && a % 100 != 0)
+                {
+                    Console.WriteLine(a + " - true");
+                }
+                else
+                {
+                    Console.WriteLine(a + " - false");
+                }
+            }
+        }
+    }
+}
